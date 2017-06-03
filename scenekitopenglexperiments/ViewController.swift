b363d9f09e40eeb990f3e8ea642d1aef31a97528
stanchiang <stanchiang23@gmail.com>
2017-06-03 13:50:04 -0700

    added markernodes

--- scenekitopenglexperiments/ViewController.swift
+++ scenekitopenglexperiments/ViewController.swift
@@ -18,9 +18,10 @@ class ViewController: UIViewController {
         view.showsStatistics = true
         return view
     }()
-    
-    lazy var scene:SCNScene = {
+
 //        http://iosdeveloperzone.com/2016/05/02/using-scenekit-and-coremotion-in-swift/
+    lazy var scene:SCNScene = {
+
         guard let imagePath = Bundle.main.path(forResource: "Hellbrunn25", ofType: "jpg") else {
             fatalError("Failed to find path for panaromic file.")
         }
@@ -45,19 +46,72 @@ class ViewController: UIViewController {
         return scene
     }()
     
+//    lazy var sphere: SCNSphere = {
+//        guard let imagePath = Bundle.main.path(forResource: "Hellbrunn25", ofType: "jpg") else {
+//            fatalError("Failed to find path for panaromic file.")
+//        }
+//        guard let image = UIImage(contentsOfFile:imagePath) else {
+//            fatalError("Failed to load panoramic image")
+//        }
+//        
+//        let sphere = SCNSphere(radius: 20.0)
+//        sphere.firstMaterial!.isDoubleSided = true
+//        sphere.firstMaterial!.diffuse.contents = image
+//        
+//        return sphere
+//    }()
+//    
+//    lazy var sphereNode: SCNNode = {
+//        let node = SCNNode(geometry: sphere)
+//        node.position = SCNVector3Make(0,0,0)
+//        scene.rootNode.addChildNode(node)
+//        
+//        return node
+//    }()
+    
+    var markedPoints = [SCNVector3]()
+    
+    var tapGestRec = UITapGestureRecognizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.addSubview(sceneView)
         sceneView.scene = scene
         
+        tapGestRec.addTarget(self, action: #selector(tapHandler(sender:)))
+        self.view.addGestureRecognizer(tapGestRec)
     }
 
+    func tapHandler(sender: UITapGestureRecognizer) {
+        let point2d = sender.location(in: self.view)
+        print(point2d)
+        
+        let hitResults:[SCNHitTestResult] = sceneView.hitTest(point2d, options: nil)
+        
+        if let result = hitResults.first {
+            markPoint(on: result.node, at: result.localCoordinates)
+        }
+    }
+    
+    func markPoint(on sphere:SCNNode, at point3d:SCNVector3) {
+        let mark = SCNSphere(radius: 0.2)
+        mark.firstMaterial?.diffuse.contents = UIColor.cyan
+        
+        let markerNode = SCNNode(geometry: mark)
+        markerNode.position = point3d
+        markedPoints.append(markerNode.position)
+        sphere.addChildNode(markerNode)
+        
+        print(markerNode.position)
+    }
+    
     override func viewWillLayoutSubviews() {
         sceneView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
         sceneView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
         sceneView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
         sceneView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
     }
+    
 }
 
