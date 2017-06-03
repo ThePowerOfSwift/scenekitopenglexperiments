76238fa43a9c0489210f06766ac817602222439e
stanchiang <stanchiang23@gmail.com>
2017-06-03 16:14:00 -0700

    toggling refactored

--- scenekitopenglexperiments/ViewController.swift
+++ scenekitopenglexperiments/ViewController.swift
@@ -10,38 +10,23 @@ import UIKit
 import SceneKit
 
 class ViewController: UIViewController {
-
-    lazy var sphereSceneView: SCNView = {
+    
+    lazy var activeSceneView: SCNView = {
         let view = SCNView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.allowsCameraControl = true
         view.showsStatistics = true
-//        view.tag = 0
         view.isHidden = false
         return view
     }()
-    
-    lazy var extractedSceneView: SCNView = {
-        let view = SCNView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.allowsCameraControl = true
-        view.showsStatistics = true
-        view.alpha = 0.5
-//        view.tag = 1
-        view.isHidden = true
-        return view
-    }()
-    
-    var activeSceneView:SCNView?
-//    var activeSceneTag:Int?
     
     lazy var toggleButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.isEnabled = false
         button.backgroundColor = UIColor.orange
-        button.setTitle("Add more Points", for: UIControlState.disabled)
-        button.setTitle("Switch View", for: UIControlState.normal)
+        button.setTitle("Add 4 more Points", for: UIControlState.disabled)
+        button.setTitle("Switch to Extracted View", for: UIControlState.normal)
         return button
     }()
     
@@ -97,11 +82,11 @@ class ViewController: UIViewController {
     
     var markedPoints:[SCNNode] = [SCNNode]() {
         didSet {
-            if markedPoints.count == 4 {
+            if markedPoints.count < 4 {
+                toggleButton.setTitle("Add \(4 - markedPoints.count) more Points (Tap on Scene)", for: UIControlState.disabled)
+            } else if markedPoints.count == 4 {
                 toggleButton.isEnabled = true
-            }
-            
-            if markedPoints.count > 4 {
+            } else {
                 markedPoints.removeFirst().removeFromParentNode()
             }
         }
@@ -110,18 +95,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        sphereSceneView.scene = sphereScene
-        extractedSceneView.scene = extractedScene
+        self.view.addSubview(activeSceneView)
         
-        self.view.addSubview(extractedSceneView)
-        self.view.addSubview(sphereSceneView)
-        
-//        activeSceneTag = 0
-        
-        activeSceneView = sphereSceneView
-//        if let activeView = activeSceneView {
-//            self.view.addSubview(activeView)
-//        }
+        activeSceneView.scene = sphereScene
         
         self.view.addGestureRecognizer(tapGestRec)
         
@@ -131,35 +107,25 @@ class ViewController: UIViewController {
 
     ///the gesture to handle convert the cgpoint tap location to the location in the 360 sphere
     func tapHandler(sender: UITapGestureRecognizer) {
-        guard let activeView = activeSceneView else {
-            fatalError("No active scene")
+        guard let activeScene = activeSceneView.scene else {
+            fatalError("no active scene")
         }
-        
-        guard activeView == sphereSceneView else {
+
+        guard activeScene == sphereScene else {
             print("marks can only be added to the sphere scene; you are not on the sphere scene")
             return
         }
         
-//        guard let tag = activeSceneTag else {
-//            fatalError("No active scene tag")
-//        }
-//        
-//        guard tag == 0 else {
-//            print("marks are only allowed on the sphere view")
-//            return
-//        }
-        
         let point2d = sender.location(in: self.view)
-        print(point2d)
         
-        var hitResults:[SCNHitTestResult] = activeView.hitTest(point2d, options: nil)
+        let hitResults:[SCNHitTestResult] = activeSceneView.hitTest(point2d, options: nil)
         
         if let result = hitResults.first {
             markPoint(on: result.node, at: result.localCoordinates)
         }
     }
     
-    
+
     ///adds point to 360 image
     func markPoint(on sphere:SCNNode, at point3d:SCNVector3) {
         let mark = SCNSphere(radius: 0.2)
@@ -169,72 +135,27 @@ class ViewController: UIViewController {
         markerNode.position = point3d
         sphere.addChildNode(markerNode)
         markedPoints.append(markerNode)
-        print(markerNode.position)
     }
     
     ///toggles between the 360 sphere and the scene that will contain the outputed texture
     func toggleHandler(sender: UIButton) {
         
-        guard let activeView = activeSceneView else {
-            fatalError("No active scene")
+        if let activeScene = activeSceneView.scene {
+            if activeScene == sphereScene {
+                activeSceneView.scene = extractedScene
+                toggleButton.setTitle("Switch to Sphere View", for: UIControlState.normal)
+            } else {
+                activeSceneView.scene = sphereScene
+                toggleButton.setTitle("Switch to Extracted View", for: UIControlState.normal)
+            }
         }
-        
-//        guard let tag = activeSceneTag else {
-//            fatalError("No active scene tag")
-//        }
-        
-        print("swap scenes")
-        
-//        if activeView == sphereSceneView {
-            sphereSceneView.isHidden = !sphereSceneView.isHidden
-            extractedSceneView.isHidden = !extractedSceneView.isHidden
-//        } else if activeView == extractedSceneView {
-//            sphereSceneView.isHidden = !sphereSceneView.isHidden
-//            extractedSceneView.isHidden = !extractedSceneView.isHidden
-//        }
-        
-//        self.view.viewWithTag(0)?.isHidden = !(self.view.viewWithTag(0)?.isHidden)!
-//        self.view.viewWithTag(1)?.isHidden = !(self.view.viewWithTag(1)?.isHidden)!
-
-//        if tag == 0 {
-//            activeSceneTag = 1
-//        } else {
-//            activeSceneTag = 0
-//        }
-        
-        
-//        print(activeSceneView!.tag)
-//        if activeView.tag == 0 {
-//            activeSceneView = extractedSceneView
-//            reloadInputViews()
-//            print(activeSceneView!.tag)
-//        } else {
-//            activeSceneView = sphereSceneView
-//            reloadInputViews()
-//            print(activeSceneView!.tag)
-//        }
-        
-        
     }
     
     override func viewWillLayoutSubviews() {
-//        if let activeView = activeSceneView {
-//            activeView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-//            activeView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-//            activeView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
-//            activeView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
-//        }
-        
-        sphereSceneView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        sphereSceneView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-        sphereSceneView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
-        sphereSceneView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
-        
-        extractedSceneView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        extractedSceneView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-        extractedSceneView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
-        extractedSceneView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
-        
+        activeSceneView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        activeSceneView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        activeSceneView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        activeSceneView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
     }
     
     override var prefersStatusBarHidden: Bool {
