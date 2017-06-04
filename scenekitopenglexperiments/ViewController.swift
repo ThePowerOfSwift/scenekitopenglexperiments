f3ee5f960b0231db080e3596489153bc1aa36732
stanchiang <stanchiang23@gmail.com>
2017-06-03 20:39:09 -0700

    turned marked points into a 2d plane node

--- scenekitopenglexperiments/ViewController.swift
+++ scenekitopenglexperiments/ViewController.swift
@@ -62,21 +62,8 @@ class ViewController: UIViewController {
     lazy var extractedScene:SCNScene = {
         //TODO: showing the source sphere as done below is only to serve as a placeholder. once we have extracted textures from the sphere scene, we should dump them in this scene
         
-        guard let imagePath = Bundle.main.path(forResource: "gallery", ofType: "jpeg") else {
-            fatalError("Failed to find path for panaromic file.")
-        }
-        guard let image = UIImage(contentsOfFile:imagePath) else {
-            fatalError("Failed to load panoramic image")
-        }
         let scene = SCNScene()
         
-        let sphere = SCNSphere(radius: 20.0)
-        sphere.firstMaterial!.isDoubleSided = true
-        sphere.firstMaterial!.diffuse.contents = image
-        
-        let sphereNode = SCNNode(geometry: sphere)
-        sphereNode.position = SCNVector3Make(0,0,0)
-        scene.rootNode.addChildNode(sphereNode)
         
         return scene
     }()
@@ -97,15 +84,19 @@ class ViewController: UIViewController {
             
             if markedPoints.count == 4 {
                 toggleButton.isEnabled = true
+                toggleButton.backgroundColor = UIColor.green
                 drawLine(between: markedPoints[markedPoints.count - 1], and: markedPoints[markedPoints.count - 2])
                 drawLine(between: markedPoints[markedPoints.count - 1], and: markedPoints[0])
+                
+                extractTexture(from: markedPoints)
+                createPlane(with: markedPoints)
             }
             else {
                 markedPoints.removeFirst().removeFromParentNode()
                 //TODO:update lines for debug visualization purposes only
             }
             
-            extractTexture(from: markedPoints)
+            
         }
     }
     
@@ -119,7 +110,6 @@ class ViewController: UIViewController {
         lineShape.materials.first?.diffuse.contents = UIColor.cyan
         
         let lineNode = SCNNode(geometry: lineShape)
-        
         
         nodeA.parent!.addChildNode(lineNode)
     }
@@ -155,30 +145,31 @@ class ViewController: UIViewController {
             markPoint(on: result.node, at: result.localCoordinates)
         }
     }
-    
 
     ///adds point to 360 image
-    func markPoint(on sphere:SCNNode, at point3d:SCNVector3) {
-        let mark = SCNSphere(radius: 0.3)
-        mark.firstMaterial?.diffuse.contents = UIColor.cyan
-        
-        let markerNode = SCNNode(geometry: mark)
-        markerNode.position = point3d
-        sphere.addChildNode(markerNode)
-        markedPoints.append(markerNode)
-    }
-    
-    func markPoint(on sphere:SCNNode, at point3d:SCNVector3, with color:UIColor) {
+    func markPoint(on sphere:SCNNode, at point3d:SCNVector3, with color:UIColor = UIColor.cyan) {
         let mark = SCNSphere(radius: 0.3)
         mark.firstMaterial?.diffuse.contents = color
         
         let markerNode = SCNNode(geometry: mark)
         markerNode.position = point3d
         sphere.addChildNode(markerNode)
+        
+        if markedPoints.count <= 4 {
+            markedPoints.append(markerNode)
+        }
     }
     
     ///toggles between the 360 sphere and the scene that will contain the outputed texture
     func toggleHandler(sender: UIButton) {
+        toggle()
+    }
+    
+    func toggleHandler() {
+        toggle()
+    }
+    
+    func toggle(){
         if let activeScene = activeSceneView.scene {
             if activeScene == sphereScene {
                 activeSceneView.scene = extractedScene
@@ -201,13 +192,36 @@ class ViewController: UIViewController {
         })
         
         //FIXME: test edge case - if i set marked points very far away from each other (like on opposite sides of the sphere) the projected points have off screen CGpoint values that are not converted back into 3d properly. we can re-convert these 2d points into 3d space then we can loop through those 2d coordinates grabbing the projected 2d pixels without having to deal with the polar coordinates with the sphere. for example, https://stackoverflow.com/a/26945594/1079379
+        // for now it only works if marked points are somewhat close together like if all 4 points are visible at the same time, without needing to adjust the camera
         
-        for point2d in projectedPoints {
-            let hitResults:[SCNHitTestResult] = activeSceneView.hitTest(point2d, options: nil)
-            if let result = hitResults.first {
-                markPoint(on: result.node, at: result.localCoordinates, with: UIColor.red)
-            }
+//        for point2d in projectedPoints {
+//            let hitResults:[SCNHitTestResult] = activeSceneView.hitTest(point2d, options: nil)
+//            if let result = hitResults.first {
+//                markPoint(on: result.node, at: result.localCoordinates, with: UIColor.red)
+//            }
+//        }
+    }
+    
+    func createPlane(with nodes:[SCNNode]) {
+        let positions:[SCNVector3] = nodes.map({$0.position as SCNVector3})
+        let triangles:[[Int32]] = [[0, 1, 2], [2, 3, 0]]
+        var elements:[SCNGeometryElement] = [SCNGeometryElement]()
+        
+        let vertexSource:SCNGeometrySource = SCNGeometrySource(vertices: positions, count: positions.count)
+        
+        for triangle in triangles {
+            elements.append(SCNGeometryElement(indices: triangle, primitiveType: SCNGeometryPrimitiveType.triangles))
         }
+        
+        let planeShape = SCNGeometry(sources: [vertexSource], elements: elements)
+        planeShape.materials.first?.diffuse.contents = UIColor.cyan
+        planeShape.materials.first?.isDoubleSided = true
+        
+        let planeNode = SCNNode(geometry: planeShape)
+        planeNode.position = SCNVector3Zero
+
+        extractedScene.rootNode.addChildNode(planeNode)
+        
     }
     
     func classifyCorners(from nodes:[SCNNode]) -> [String:SCNVector3] {
