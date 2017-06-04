4938340c669cad2113186b7c4249a72052337343
stanchiang <stanchiang23@gmail.com>
2017-06-03 17:32:51 -0700

    added placeholders to fill later where we classify the 4 dots into convex hull to grab the right texture

--- scenekitopenglexperiments/ViewController.swift
+++ scenekitopenglexperiments/ViewController.swift
@@ -25,20 +25,22 @@ class ViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.isEnabled = false
         button.backgroundColor = UIColor.orange
-        button.setTitle("Add 4 more Points", for: UIControlState.disabled)
+        button.setTitle("Add 4 more Points (Tap on Scene)", for: UIControlState.disabled)
         button.setTitle("Switch to Extracted View", for: UIControlState.normal)
+        button.addTarget(self, action: #selector(toggleHandler(sender:)), for: UIControlEvents.touchUpInside)
         return button
     }()
     
 //        http://iosdeveloperzone.com/2016/05/02/using-scenekit-and-coremotion-in-swift/
     lazy var sphereScene:SCNScene = {
 
-        guard let imagePath = Bundle.main.path(forResource: "Hellbrunn25", ofType: "jpg") else {
+        guard let imagePath = Bundle.main.path(forResource: "gallery", ofType: "jpeg") else {
             fatalError("Failed to find path for panaromic file.")
         }
         guard let image = UIImage(contentsOfFile:imagePath) else {
             fatalError("Failed to load panoramic image")
         }
+        
         let scene = SCNScene()
         
         let sphere = SCNSphere(radius: 20.0)
@@ -59,7 +61,7 @@ class ViewController: UIViewController {
     
     lazy var extractedScene:SCNScene = {
         
-        guard let imagePath = Bundle.main.path(forResource: "Hellbrunn25", ofType: "jpg") else {
+        guard let imagePath = Bundle.main.path(forResource: "gallery", ofType: "jpeg") else {
             fatalError("Failed to find path for panaromic file.")
         }
         guard let image = UIImage(contentsOfFile:imagePath) else {
@@ -84,25 +86,50 @@ class ViewController: UIViewController {
         didSet {
             if markedPoints.count < 4 {
                 toggleButton.setTitle("Add \(4 - markedPoints.count) more Points (Tap on Scene)", for: UIControlState.disabled)
-            } else if markedPoints.count == 4 {
+                
+                if markedPoints.count > 1 {
+                    drawLine(between: markedPoints[markedPoints.count - 1], and: markedPoints[markedPoints.count - 2])
+                }
+                
+                return
+            }
+            
+            if markedPoints.count == 4 {
                 toggleButton.isEnabled = true
+                drawLine(between: markedPoints[markedPoints.count - 1], and: markedPoints[markedPoints.count - 2])
+                drawLine(between: markedPoints[markedPoints.count - 1], and: markedPoints[0])
             } else {
                 markedPoints.removeFirst().removeFromParentNode()
             }
+            
+            extractTexture(from: markedPoints)
         }
+    }
+    
+    func drawLine(between nodeA:SCNNode, and nodeB:SCNNode) {
+        let indices: [Int32] = [0, 1]
+        
+        let source = SCNGeometrySource(vertices: [nodeA.position, nodeB.position], count: 2)
+        let element = SCNGeometryElement(indices: indices, primitiveType: .line)
+        
+        let lineShape = SCNGeometry(sources: [source], elements: [element])
+        
+        let lineNode = SCNNode(geometry: lineShape)
+        
+        
+        nodeA.parent!.addChildNode(lineNode)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addSubview(activeSceneView)
-        
         activeSceneView.scene = sphereScene
+        self.view.addSubview(activeSceneView)
         
         self.view.addGestureRecognizer(tapGestRec)
         
         self.view.addSubview(toggleButton)
-        toggleButton.addTarget(self, action: #selector(toggleHandler(sender:)), for: UIControlEvents.touchUpInside)
+        
     }
 
     ///the gesture to handle convert the cgpoint tap location to the location in the 360 sphere
@@ -117,11 +144,11 @@ class ViewController: UIViewController {
         }
         
         let point2d = sender.location(in: self.view)
-        
         let hitResults:[SCNHitTestResult] = activeSceneView.hitTest(point2d, options: nil)
         
         if let result = hitResults.first {
             markPoint(on: result.node, at: result.localCoordinates)
+            print(result.localCoordinates)
         }
     }
     
@@ -139,7 +166,6 @@ class ViewController: UIViewController {
     
     ///toggles between the 360 sphere and the scene that will contain the outputed texture
     func toggleHandler(sender: UIButton) {
-        
         if let activeScene = activeSceneView.scene {
             if activeScene == sphereScene {
                 activeSceneView.scene = extractedScene
@@ -149,6 +175,23 @@ class ViewController: UIViewController {
                 toggleButton.setTitle("Switch to Extracted View", for: UIControlState.normal)
             }
         }
+    }
+    
+    func extractTexture(from nodes:[SCNNode]) {
+        let processedNodes = classifyCorners(from: nodes)
+        
+        for (key, node) in processedNodes {
+            //set colors for the corners
+        }
+    }
+    
+    func classifyCorners(from nodes:[SCNNode]) -> [String:SCNVector3] {
+        let positions:[SCNVector3] = nodes.map({($0 as SCNNode).position})
+        
+        //TODO: implement quickhull to classify corners
+        
+        let planeDict:[String:SCNVector3] = ["tl":positions[0], "tr":positions[1], "br":positions[2], "bl":positions[3]]
+        return planeDict
     }
     
     override func viewWillLayoutSubviews() {
