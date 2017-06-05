a09ea2fd3e8fc6d14d074d2b3a7fe462a9577d18
stanchiang <stanchiang23@gmail.com>
2017-06-04 22:24:14 -0700

    allow saving of multiple planes to the extracted scene that can share the same corners, this even means there might be duplicate overlapping planes as an edge case

--- scenekitopenglexperiments/ViewController.swift
+++ scenekitopenglexperiments/ViewController.swift
@@ -49,6 +49,7 @@ class ViewController: UIViewController {
         
         let sphereNode = SCNNode(geometry: sphere)
         sphereNode.position = SCNVector3Make(0,0,0)
+        sphereNode.name = "sphereScene"
         scene.rootNode.addChildNode(sphereNode)
         
         let cameraNode = SCNNode()
@@ -59,44 +60,49 @@ class ViewController: UIViewController {
         return scene
     }()
     
-    lazy var extractedScene:SCNScene = {
-        //TODO: showing the source sphere as done below is only to serve as a placeholder. once we have extracted textures from the sphere scene, we should dump them in this scene
-        
-        let scene = SCNScene()
-        
-        
-        return scene
-    }()
+    lazy var extractedScene:SCNScene = SCNScene()
     
     lazy var tapGestRec:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapHandler(sender:)))
     
-    var markedPoints:[SCNNode] = [SCNNode]() {
+    ///planes are stored as an array of vertex array indicies
+    var markedPlanes:[[Int]] = [[Int]]() {
         didSet {
-            if markedPoints.count < 4 {
-                toggleButton.setTitle("Add \(4 - markedPoints.count) more Points (Tap on Scene)", for: UIControlState.disabled)
+            if markedPlanes.last!.count % 4 == 0 {
+                let newCompleteSet:[SCNNode] = markedPlanes.last!.map({markedVertices[$0]})
+                let texture = extractTexture(from: newCompleteSet)
+                let plane = createPlane(with: newCompleteSet)
+                applyTexture(from: texture, to: plane)
                 
-                if markedPoints.count > 1 {
-                    drawLine(between: markedPoints[markedPoints.count - 1], and: markedPoints[markedPoints.count - 2])
-                }
-                
-                return
+                toggleButton.sendActions(for: .touchUpInside)
+            }
+        }
+    }
+    
+    ///vertices are stored as a master array of unique nodes
+    var markedVertices:[SCNNode] = [SCNNode]() {
+        didSet {
+            //TODO: we should first check if there already exists a line and only draw a line between nodes that were previously not connected
+            
+            //FIXME: line drawing is not correct - low priority defect; for visualization only
+            //connect new node to previous node when not on the first node of a new plane
+//            if markedPlanes[markedPlanes.count - 1].count > 0 {
+//                drawLine(between: markedVertices[markedVertices.count - 1], and: markedVertices[markedVertices.count - 2])
+//            }
+//            
+//            //also anchor 4th corner to the first node
+//            if markedVertices.count == 4 {
+//                drawLine(between: markedVertices[markedVertices.count - 1], and: markedVertices[0])
+//            }
+
+            //we need more points to create our first plane
+            if markedVertices.count < 4 {
+                toggleButton.setTitle("Add \(4 - markedVertices.count) more Points (Tap on Scene)", for: UIControlState.disabled)
             }
             
-            if markedPoints.count == 4 {
+            if markedVertices.count == 4 {
                 toggleButton.isEnabled = true
                 toggleButton.backgroundColor = UIColor.green
-                drawLine(between: markedPoints[markedPoints.count - 1], and: markedPoints[markedPoints.count - 2])
-                drawLine(between: markedPoints[markedPoints.count - 1], and: markedPoints[0])
-                
-                extractTexture(from: markedPoints)
-                createPlane(with: markedPoints)
             }
-            else {
-                markedPoints.removeFirst().removeFromParentNode()
-                //TODO:update lines for debug visualization purposes only
-            }
-            
-            
         }
     }
     
@@ -138,38 +144,81 @@ class ViewController: UIViewController {
         }
         
         let point2d = sender.location(in: self.view)
-        print(point2d)
+//        print(point2d)
         let hitResults:[SCNHitTestResult] = activeSceneView.hitTest(point2d, options: nil)
         
-        if let result = hitResults.first {
-            markPoint(on: result.node, at: result.localCoordinates)
-        }
+        parseResults(hitResults: hitResults)
     }
 
+    func parseResults(hitResults: [SCNHitTestResult]) {
+        //make sure there's at least 1 result and we assume its the sphere
+        guard let firstResult = hitResults.first else {
+            fatalError("camera should be inside sphere so taps should always be hitting at least 1 node, not empty space")
+        }
+        
+        //if there are 2 results make sure the second one is the sphere
+        if hitResults.count == 2 {
+            guard hitResults[1].node.name == "sphereScene" else {
+                fatalError("Broken Assumption: we assume that if we hit 2 items the first is always an exsitng marked vertex node and the second is the main sphere scene. but based on the name, the second result is not a sphere")
+            }
+        } else if hitResults.count > 2 { //more than 2 results
+            fatalError("the hittest hit \(hitResults.count) nodes (1 vertex, 1 main sphere). we don't want to support overlapping vertices right now so we will crash the app")
+        }
+        
+        //if we are still identifing our first plane
+        if markedVertices.count < 4 {
+            //if we just selected our first vertex directly add it to array
+            markVertex(on: firstResult.node, at: firstResult.localCoordinates)
+            
+            //if first plane then we know the index of the first 4 vertices
+            if markedPlanes.count == 0 {
+                markedPlanes.append([0])
+            } else {
+                markedPlanes[0].append(markedVertices.count - 1)
+            }
+        } else { // if there's already at least 1 plane then we have to consider that the user selected an existing node as the corner of a second plane
+            //if selected node is an existing node, then add their existing index in the vertex array to the planes array without adding any new vertices
+            var nodeIndex:Int?
+            
+            if let indexOfFoundNode = markedVertices.index(of: firstResult.node) {
+                nodeIndex = indexOfFoundNode
+            } else { //if selected node is a new vertex on the sphere, then add record it to the array
+                
+                if hitResults.count == 2 {
+                    
+                }
+                markVertex(on: firstResult.node, at: firstResult.localCoordinates, with: UIColor.purple)
+                nodeIndex = markedVertices.count - 1
+            }
+            
+            guard let index = nodeIndex else {
+                fatalError("something went wrong with index selection")
+            }
+            
+            //add that index to the plane that is currently being constructed
+            if markedPlanes.last!.count % 4 != 0 {
+                markedPlanes[markedPlanes.count - 1].append(index)
+            } else { //or new node index to the current plane's indicies array or start storing into a new plane
+                markedPlanes.append([index])
+            }
+        }
+    }
+    
     ///adds point to 360 image
-    func markPoint(on sphere:SCNNode, at point3d:SCNVector3, with color:UIColor = UIColor.cyan) {
-        let mark = SCNSphere(radius: 0.3)
+    func markVertex(on sphere:SCNNode, at point3d:SCNVector3, with color:UIColor = UIColor.cyan) {
+        let mark = SCNSphere(radius:
+//            1)
+            0.3)
         mark.firstMaterial?.diffuse.contents = color
         
         let markerNode = SCNNode(geometry: mark)
         markerNode.position = point3d
         sphere.addChildNode(markerNode)
-        
-        if markedPoints.count <= 4 {
-            markedPoints.append(markerNode)
-        }
+        markedVertices.append(markerNode)
     }
     
     ///toggles between the 360 sphere and the scene that will contain the outputed texture
     func toggleHandler(sender: UIButton) {
-        toggle()
-    }
-    
-    func toggleHandler() {
-        toggle()
-    }
-    
-    func toggle(){
         if let activeScene = activeSceneView.scene {
             if activeScene == sphereScene {
                 activeSceneView.scene = extractedScene
@@ -181,13 +230,13 @@ class ViewController: UIViewController {
         }
     }
     
-    func extractTexture(from nodes:[SCNNode]) {
+    func extractTexture(from nodes:[SCNNode]) -> SCNMaterial {
 //        let processedNodes = classifyCorners(from: nodes)
         
         let projectedPoints:[CGPoint] = nodes.map({
             let projPoint = activeSceneView.projectPoint($0.position as SCNVector3)
             let flattenedPoint = CGPoint(x: CGFloat(projPoint.x), y: CGFloat(projPoint.y))
-            print(flattenedPoint)
+//            print(flattenedPoint)
             return flattenedPoint
         })
         
@@ -200,9 +249,10 @@ class ViewController: UIViewController {
 //                markPoint(on: result.node, at: result.localCoordinates, with: UIColor.red)
 //            }
 //        }
+        return SCNMaterial()
     }
     
-    func createPlane(with nodes:[SCNNode]) {
+    func createPlane(with nodes:[SCNNode]) -> SCNNode {
         let positions:[SCNVector3] = nodes.map({$0.position as SCNVector3})
         let triangles:[[Int32]] = [[0, 1, 2], [2, 3, 0]]
         var elements:[SCNGeometryElement] = [SCNGeometryElement]()
@@ -214,14 +264,18 @@ class ViewController: UIViewController {
         }
         
         let planeShape = SCNGeometry(sources: [vertexSource], elements: elements)
-        planeShape.materials.first?.diffuse.contents = UIColor.cyan
+        planeShape.materials.first?.diffuse.contents = markedPlanes.count == 1 ? UIColor.cyan : UIColor.purple
         planeShape.materials.first?.isDoubleSided = true
         
+        //FIXME: we need the reverse perspective transformed version of the plane instead of directly using the trapezoidal plane if we want proper depth
         let planeNode = SCNNode(geometry: planeShape)
-        planeNode.position = SCNVector3Zero
-
-        extractedScene.rootNode.addChildNode(planeNode)
+        return planeNode
         
+    }
+    
+    func applyTexture(from texture:SCNMaterial, to node:SCNNode) {
+//        TODO: also apply the texture we grabbed to the node that we cut out. although having some kind of scenekit or opengl function that directly grabbed plane and texture together would be even better
+        extractedScene.rootNode.addChildNode(node)
     }
     
     func classifyCorners(from nodes:[SCNNode]) -> [String:SCNVector3] {
@@ -232,6 +286,7 @@ class ViewController: UIViewController {
 //        https://github.com/utahwithak/QHullSwift
 //        https://gist.github.com/adunsmoor/e848356a57980ab9f822
         let planeDict:[String:SCNVector3] = ["tl":positions[0], "tr":positions[1], "br":positions[2], "bl":positions[3]]
+
         return planeDict
     }
     
@@ -247,4 +302,5 @@ class ViewController: UIViewController {
             return true
         }
     }
+    
 }
